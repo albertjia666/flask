45c4c15072eb63403e8517e401281445a2f7b594
albertjia666 <airborne101506@163.com>
2022-02-16 10:47:27 +0800

    app.py

--- stop.sh
+++ stop.sh
@@ -5,8 +5,8 @@
 flask_pid=`ps -ef | grep flask | grep -v grep | awk '{print $2}'`
 
 if [ $flask_pid ]; then
-    echo "flask running... stopping..."
+    echo "flask running... restarting..."
     kill -9 $flask_pid
 else
-    echo "flask not running..."
+    echo "flask not running... starting..."
 fi
