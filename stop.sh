46191bd74ecdd7e0862d95a992a73fac28ee6c30
albertjia666 <airborne101506@163.com>
2022-02-16 10:39:53 +0800

    app.py

--- stop.sh
+++ stop.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ps -ef | grep flask | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+# ps -ef | grep flask | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+
+flask_pid=`ps -ef | grep flask | grep -v grep | awk '{print $2}'`
+
+if [ $flask_pid ]; then
+    echo "flask running... stopping..."
+    kill -9 $flask_pid
+else
+    echo "flask not running..."
+fi
